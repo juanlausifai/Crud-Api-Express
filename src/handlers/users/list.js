d9ab42008838900cea3d17b0d10bd4a9f4d9123c
Juan Martinez <juan.martinez@patagonian.it>
2021-05-03 18:38:09 -0300

    Agrega filtrado en users

--- src/handlers/users/list.js
+++ src/handlers/users/list.js
@@ -1,8 +1,15 @@
-const database = require('../../database');
+const database = require("../../database");
 
 // GET /api/users
 module.exports = (route) => {
-  route.get('/', (req, res) => {
-    res.json(database.DB);
+  route.get("/", (req, res) => {
+    let filterName = req.query.filterName;
+
+    let users = database.DB;
+    if (filterName) {
+      users = users.filter((elem) => elem.name.includes(filterName));
+    }
+
+    res.json(users);
   });
 };
