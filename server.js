f1506241eccbca35538c0f4cf43020da6c5ca661
Eduardo Cuomo <reduardo7@gmail.com>
2021-05-03 19:11:31 -0300

    Publicar directorio "public".

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routingAPI);
+app.use(express.static('./public'));
 
 app.listen(PORT, () => {
   console.info(`Escuchando en puerto ${PORT}`);
